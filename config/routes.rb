95ce3a1abfd7243d9403dcfddc92a8bdfdb2f341
Thiago Pinto <tapgyn@gmail.com>
2013-01-02 15:50:13 -0500

    routes root to canvas

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Game1::Application.routes.draw do
-  root to: "home#index"
+  root to: "home#web"
   get 'mobile' => 'home#mobile'
   get 'web' => 'home#web'
+  get 'index' => 'home#index'
 
   match "fb_canvas" => "home#web"
 
